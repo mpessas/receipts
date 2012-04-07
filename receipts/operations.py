d092df4f18d3ccb76326f2064502f6afacce94d5
Apostolos Bessas <mpessas@transifex.com>
2012-04-07 20:11:22 +0300

    Implement saving a receipt.

--- receipts/operations.py
+++ receipts/operations.py
@@ -4,13 +4,15 @@
 Operations supported by ``receipts``.
 """
 
+from receipts.core_types import Receipt
+
 
 def add_receipt(**kwargs):
     """Add a receipt to the database."""
     vat = kwargs['vat']
     price = kwargs['price']
     when = kwargs['when']
-    print(vat, price, when)
+    Receipt(vat, price, when).save()
 
 
 def report_month(**kwargs):
